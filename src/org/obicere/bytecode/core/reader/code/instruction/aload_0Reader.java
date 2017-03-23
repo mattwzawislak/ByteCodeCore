05335f90675265b5dd3d0449ae2dca68afe3a32e
Obicere <mattwzawislak@gmail.com>
2017-03-23 00:47:09 -0600

    Renaming and some other small changes.

--- src/org/obicere/bytecode/core/reader/code/instruction/aload_0Reader.java
+++ src/org/obicere/bytecode/core/reader/code/instruction/aload_0Reader.java
@@ -1,5 +1,6 @@
 package org.obicere.bytecode.core.reader.code.instruction;
 
+import org.javacore.code.instruction.ALoad_0;
 import org.obicere.bytecode.core.objects.code.instruction.DefaultALoad_0;
 import org.obicere.bytecode.core.reader.Reader;
 import org.obicere.bytecode.core.util.ByteCodeReader;
@@ -9,10 +10,10 @@ import java.io.IOException;
 /**
  * @author Obicere
  */
-public class aload_0Reader implements Reader<DefaultALoad_0> {
+public class ALoad_0Reader implements Reader<ALoad_0> {
 
     @Override
-    public DefaultALoad_0 read(final ByteCodeReader input) throws IOException {
+    public ALoad_0 read(final ByteCodeReader input) throws IOException {
         return DefaultALoad_0.INSTANCE;
     }
 }
